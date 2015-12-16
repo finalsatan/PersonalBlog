fb57f08ea2f61049ff3814ed9e838e9a27048017
finalsatan <zhangyhhust@gmail.com>
2015-12-16 17:22:12 +0800

    Update app.py

--- www/app.py
+++ www/app.py
@@ -135,7 +135,7 @@ def datatime_filter( t ):
         return u'%s小时前' % ( delta // 3600 )
     if delta < 604800:
         return u'%s天前' % ( delta // 86400 )
-    dt = datatime.fromtimestamp( t )
+    dt = datetime.fromtimestamp( t )
     return u'%s年%s月%s日' % ( dt.year, dt.month, dt.day )
 
 @asyncio.coroutine
